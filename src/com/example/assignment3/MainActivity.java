9d65c34960b54035815924cf20e4acae0c15b03a
blarmon <christopher.siegel@drake.edu>
2014-09-11 08:09:08 -0500

    Update MainActivity.java

--- src/com/example/assignment3/MainActivity.java
+++ src/com/example/assignment3/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 //Button is for chris to link app
 public class MainActivity extends ActionBarActivity {
 	
